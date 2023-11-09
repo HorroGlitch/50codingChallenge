7e826c18db493ab0792720a0ba4ec13902ce0ba5
HorroGlitch <lverot@edenschool.fr>
2023-11-09 16:48:07 +0100

    first commit and first algo

---
+++ main.js
@@ -0,0 +1,46 @@
+// 1.
+
+// function oneToTen(){
+//     for (let i = 0; i < 11; i++) {
+//         console.log(i);
+//     }
+// }
+// oneToTen();
+
+
+// 2.
+
+// function onlyOdd(){
+//     for (let i = 0; i < 101; i++) {
+//         if (i%2) {
+//             console.log(i);
+//         }
+        
+//     }
+// }
+// onlyOdd();
+
+
+// 3.
+
+// function sevenTable(){
+//     for (let i = 7; i < 71; i+=7) {
+       
+//             console.log(i);
+        
+        
+//     }
+// }
+// sevenTable();
+
+
+// 4.
+
+function OnetoTenTable(){
+    for (let i = 0; i < 100; i++) {
+        
+        
+        
+    }
+}
+OnetoTenTable();
